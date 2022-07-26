3ae9dec066c94a1d1cb55baa4d05081ef97ac5b3
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2022-07-26 20:53:55 -0300

    req 03

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,24 +1,25 @@
 const jwt = require('jsonwebtoken');
 
-const models = require('../database/models');
+const { User } = require('../database/models');
 
 const secret = process.env.JWT_SECRET;
 
 const loginService = {
-  async login(email, password) {
-    if (!email || !password) {
+  async login(email, countersign) {
+    if (!email || !countersign) {
       return { code: 400, data: { message: 'Some required fields are missing' } };
     }
 
-    const user = await models.User.findOne({
-      where: { email, password },
+    const user = await User.findOne({
+      where: { email, password: countersign },
+      attributes: { exclude: ['createdAt', 'updatedAt'] },
     });
 
-    if (!user) return { code: 400, data: { message: 'Invalid fields' } };
-    const { id } = user;
-    const token = jwt.sign({ data: id }, secret);
+      if (!user) return { code: 400, data: { message: 'Invalid fields' } };
+      const { id } = user;
+      const token = jwt.sign({ data: id }, secret);
 
-    return { code: 200, data: { token } };
+      return { code: 200, data: { token } };
   },
 };
 
